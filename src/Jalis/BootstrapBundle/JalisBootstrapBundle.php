78811f9fce5071e1dbc50e0d5fad18d3b9814775
root <root@usuario-HP.(none)>
2012-08-29 11:06:44 +0200

    readme and gitignore

---
+++ src/Jalis/BootstrapBundle/JalisBootstrapBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jalis\BootstrapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JalisBootstrapBundle extends Bundle
+{
+}
---
+++ src/Jalis/FoganBundle/JalisFoganBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jalis\FoganBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JalisFoganBundle extends Bundle
+{
+}
